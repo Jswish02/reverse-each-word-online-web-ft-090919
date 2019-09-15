0cb9ea840ab19a3fbe7de59d9d1b83f06cd38c0b
Jonah Brown <jswish02@gmail.com>
2019-09-15 00:38:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,20 @@
+
+  def reverse_each_word(string)
+  original_array = string.split(" ")
+  original_array.map do |orignal|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
 def reverse_each_word(string)
-  
+  array = string.split(" ") 
+  test_array = []
+  array.map do|string| 
+    test_array << string.reverse
+  end
+  test_array.join(" ")
+end
+
+
+reverse_each_word("Hello there, and how are you?")
